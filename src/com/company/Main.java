1809fc9b2755a2e069e6e5a786395ed73d887004
Gasayni <98342807+Gasayni@users.noreply.github.com>
2022-01-26 18:34:22 +0300

    Убрал все лишнее в brutForse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -297,39 +297,6 @@ public class Main {
                 }
             }
         }
-
-        // До этого условия все равно не доходит
-        /*for (shift = -6; shift < 41; shift++) {
-            s_Crypto = toCrypto_noCase(cryptoLine);  // Сдвинули каждый символ на "shift"
-            String[] mas_Crypto = s_Crypto.split("");
-//            Считаем отношение "." к ". "
-            float amount_dot = 0;
-            float amount_dot_space = 0;
-            float amount_comma = 0;
-            float amount_comma_space = 0;
-
-            for (int i = 0; i < mas_Crypto.length; i++) {
-                if (mas_Crypto[i].equals(".")) {
-                    amount_dot++;
-                    if (mas_Crypto[i+1].equals(" ")) {
-                        amount_dot_space++;
-                    }
-                }
-                if (mas_Crypto[i].equals(",")) {
-                    amount_comma++;
-                    if (mas_Crypto[i+1].equals(" ")) {
-                        amount_comma_space++;
-                    }
-                }
-            }
-            if (amount_dot != 0 || amount_dot_space != 0 || amount_comma != 0 || amount_comma_space != 0) {
-                if (amount_dot / amount_dot_space < 1.2 || amount_comma / amount_comma_space < 1.2) {
-                    System.out.print("Сдвиг шифра был = " + -1 * shift);
-                    writeFile("decipher_text.txt");
-                    return;
-                }
-            }
-        }*/  // До этого условия все равно не доходит
 //        Ну если совсем не получается определить, то выводим сообщение
         System.out.println("Не могу определить сдвиг, попробуйте файл с большим текстом внутри");
 /*//        Чтобы ускорить выборку сдвига, сначала сделаем перебор среди наиболее популярных чисел
